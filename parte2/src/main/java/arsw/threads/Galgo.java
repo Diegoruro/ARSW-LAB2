f0780e6a8fa900b0e77920118dc2e7acfe03c357
diegoruro <diegofernandoruizrojas@gmail.com>
2022-08-21 13:23:11 -0500

    parte 2 finalizada

--- parte2/src/main/java/arsw/threads/Galgo.java
+++ parte2/src/main/java/arsw/threads/Galgo.java
@@ -10,32 +10,49 @@ public class Galgo extends Thread {
 	private int paso;
 	private Carril carril;
 	RegistroLlegada regl;
+	private final Object lock;
+	private Boolean isPaused = false;
 
-	public Galgo(Carril carril, String name, RegistroLlegada reg) {
+	public Galgo(Carril carril, String name, RegistroLlegada reg, Object lock) {
 		super(name);
 		this.carril = carril;
 		paso = 0;
 		this.regl=reg;
+		this.lock = lock;
+	}
+
+	public void pause(){
+		this.isPaused = true;
+	}
+
+	public void resumeGalgos(){
+		this.isPaused = false;
 	}
 
 	public void corra() throws InterruptedException {
-		while (paso < carril.size()) {			
+		while (paso < carril.size()) {
+
 			Thread.sleep(100);
 			carril.setPasoOn(paso++);
 			carril.displayPasos(paso);
-			
-			if (paso == carril.size()) {						
-				carril.finish();
-				int ubicacion=regl.getUltimaPosicionAlcanzada();
-				regl.setUltimaPosicionAlcanzada(ubicacion+1);
-				System.out.println("El galgo "+this.getName()+" llego en la posicion "+ubicacion);
-				if (ubicacion==1){
-					regl.setGanador(this.getName());
+			synchronized (lock) {
+				if (paso == carril.size()) {
+					carril.finish();
+					int ubicacion = regl.getUltimaPosicionAlcanzada();
+					if (ubicacion == 1) {
+						regl.setGanador(this.getName());
+					}
+					regl.setUltimaPosicionAlcanzada(ubicacion + 1);
+					System.out.println("El galgo " + this.getName() + " llego en la posicion " + ubicacion);
+
 				}
-				
+				if (isPaused) {
+					this.lock.wait();
+				}
 			}
 		}
 	}
+
 
 
 	@Override
--- parte2/src/main/java/arsw/threads/MainCanodromo.java
+++ parte2/src/main/java/arsw/threads/MainCanodromo.java
@@ -17,6 +17,7 @@ public class MainCanodromo {
         can = new Canodromo(17, 100);
         galgos = new Galgo[can.getNumCarriles()];
         can.setVisible(true);
+        final Object lock = new Object();
 
         //Acción del botón start
         can.setStartAction(
@@ -24,7 +25,7 @@ public class MainCanodromo {
 
                     @Override
                     public void actionPerformed(final ActionEvent e) {
-						//como acción, se crea un nuevo hilo que cree los hilos
+                        //como acción, se crea un nuevo hilo que cree los hilos
                         //'galgos', los pone a correr, y luego muestra los resultados.
                         //La acción del botón se realiza en un hilo aparte para evitar
                         //bloquear la interfaz gráfica.
@@ -33,14 +34,20 @@ public class MainCanodromo {
                             public void run() {
                                 for (int i = 0; i < can.getNumCarriles(); i++) {
                                     //crea los hilos 'galgos'
-                                    galgos[i] = new Galgo(can.getCarril(i), "" + i, reg);
+                                    galgos[i] = new Galgo(can.getCarril(i), "" + i, reg, lock);
                                     //inicia los hilos
                                     galgos[i].start();
-
+                                    if (i + 1 == can.getNumCarriles()) {
+                                        try {
+                                            galgos[i].join();
+                                        } catch (InterruptedException ex) {
+                                            ex.printStackTrace();
+                                        }
+                                    }
                                 }
-                               
-				can.winnerDialog(reg.getGanador(),reg.getUltimaPosicionAlcanzada() - 1); 
+                                can.winnerDialog(reg.getGanador(), reg.getUltimaPosicionAlcanzada() - 1);
                                 System.out.println("El ganador fue:" + reg.getGanador());
+
                             }
                         }.start();
 
@@ -52,7 +59,10 @@ public class MainCanodromo {
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        System.out.println("Carrera pausada!");
+                        for (int i = 0; i < can.getNumCarriles(); i++) {
+                            galgos[i].pause();
+                        }
+                        System.out.println("Carrera Pausada!");
                     }
                 }
         );
@@ -61,6 +71,12 @@ public class MainCanodromo {
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
+                        for (int i = 0; i < can.getNumCarriles(); i++) {
+                            galgos[i].resumeGalgos();
+                        }
+                        synchronized (lock) {
+                            lock.notifyAll();
+                        }
                         System.out.println("Carrera reanudada!");
                     }
                 }
