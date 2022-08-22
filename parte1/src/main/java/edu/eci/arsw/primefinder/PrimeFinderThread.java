60e47dce48094e183c1b54a22c7eb8b93c0e34eb
Juan Felipe Aguas Pulido <juan.aguas@mail.escuelaing.edu.co>
2022-08-21 20:19:14 -0500

    Parte 1 arreglada

--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -1,30 +1,46 @@
 package edu.eci.arsw.primefinder;
 
+import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
 
 public class PrimeFinderThread extends Thread{
 
-	
+	private final Object lock;
 	int a,b;
 
 	private List<Integer> primes=new LinkedList<Integer>();
 	
-	public PrimeFinderThread(int a, int b) {
+	public PrimeFinderThread(Object lock, int a, int b) {
 		super();
+		this.lock = lock;
 		this.a = a;
 		this.b = b;
 	}
 
 	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
+		LocalDateTime now = LocalDateTime.now();
+		LocalDateTime then = now.plusSeconds(5);
+		for (int i=a;i<=b;i++){
+			synchronized (lock){
+				now = LocalDateTime.now();
+				if (now.isAfter(then)){
+					try {
+						System.out.println("Primes: " + primes.size());
+						System.out.println("Presione Enter para reiniciar el hilo");
+						lock.wait();
+						now = LocalDateTime.now();
+						then = now.plusSeconds(5);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			if (isPrime(i)) {
 				primes.add(i);
 				System.out.println(i);
 			}
 		}
-		
-		
 	}
 	
 	boolean isPrime(int n) {
